3168ccf1a15394309c88b795246b476865e93fc9
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2023-05-07 20:57:34 +0700

    add ProjectDetails

--- TelyuProject/UC_ProjectInfo.Designer.cs
+++ TelyuProject/UC_ProjectInfo.Designer.cs
@@ -28,23 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.Lproject = new System.Windows.Forms.Label();
+            this.TProjectNama = new System.Windows.Forms.Label();
+            this.LDeskripsi = new System.Windows.Forms.Label();
+            this.LDosen = new System.Windows.Forms.Label();
+            this.TDeskripsi = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TDosen = new System.Windows.Forms.Label();
+            this.LSkill = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Lproject
+            // 
+            this.Lproject.AutoSize = true;
+            this.Lproject.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lproject.Location = new System.Drawing.Point(102, 68);
+            this.Lproject.Name = "Lproject";
+            this.Lproject.Size = new System.Drawing.Size(66, 22);
+            this.Lproject.TabIndex = 1;
+            this.Lproject.Text = "Project";
+            // 
+            // TProjectNama
+            // 
+            this.TProjectNama.AutoSize = true;
+            this.TProjectNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TProjectNama.Location = new System.Drawing.Point(233, 65);
+            this.TProjectNama.Name = "TProjectNama";
+            this.TProjectNama.Size = new System.Drawing.Size(141, 25);
+            this.TProjectNama.TabIndex = 2;
+            this.TProjectNama.Text = "Nama Project";
+            // 
+            // LDeskripsi
+            // 
+            this.LDeskripsi.AutoSize = true;
+            this.LDeskripsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LDeskripsi.Location = new System.Drawing.Point(102, 133);
+            this.LDeskripsi.Name = "LDeskripsi";
+            this.LDeskripsi.Size = new System.Drawing.Size(99, 22);
+            this.LDeskripsi.TabIndex = 3;
+            this.LDeskripsi.Text = "Desctiption";
+            // 
+            // LDosen
+            // 
+            this.LDosen.AutoSize = true;
+            this.LDosen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LDosen.Location = new System.Drawing.Point(102, 194);
+            this.LDosen.Name = "LDosen";
+            this.LDosen.Size = new System.Drawing.Size(62, 22);
+            this.LDosen.TabIndex = 4;
+            this.LDosen.Text = "Dosen";
+            // 
+            // TDeskripsi
+            // 
+            this.TDeskripsi.AutoSize = true;
+            this.TDeskripsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TDeskripsi.Location = new System.Drawing.Point(234, 133);
+            this.TDeskripsi.Name = "TDeskripsi";
+            this.TDeskripsi.Size = new System.Drawing.Size(576, 22);
+            this.TDeskripsi.TabIndex = 5;
+            this.TDeskripsi.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // TDosen
+            // 
+            this.TDosen.AutoSize = true;
+            this.TDosen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TDosen.Location = new System.Drawing.Point(234, 194);
+            this.TDosen.Name = "TDosen";
+            this.TDosen.Size = new System.Drawing.Size(174, 22);
+            this.TDosen.TabIndex = 8;
+            this.TDosen.Text = "Edi Suharto, M. Kom";
+            // 
+            // LSkill
+            // 
+            this.LSkill.AutoSize = true;
+            this.LSkill.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LSkill.Location = new System.Drawing.Point(102, 257);
+            this.LSkill.Name = "LSkill";
+            this.LSkill.Size = new System.Drawing.Size(43, 22);
+            this.LSkill.TabIndex = 9;
+            this.LSkill.Text = "Skill";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(128, 118);
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(234, 257);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Project";
+            this.label1.Size = new System.Drawing.Size(66, 22);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Python";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(330, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 22);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Java";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(102, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 22);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Contract";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(234, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(279, 22);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "10 January 2023 - 10 March 2023";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(102, 372);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 22);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Chat Group";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(234, 372);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(207, 22);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "www.whatsapp.com/yolo";
             // 
             // UC_ProjectInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.LSkill);
+            this.Controls.Add(this.TDosen);
+            this.Controls.Add(this.TDeskripsi);
+            this.Controls.Add(this.LDosen);
+            this.Controls.Add(this.LDeskripsi);
+            this.Controls.Add(this.TProjectNama);
+            this.Controls.Add(this.Lproject);
             this.Name = "UC_ProjectInfo";
             this.Size = new System.Drawing.Size(912, 458);
             this.ResumeLayout(false);
@@ -54,6 +212,19 @@
 
         #endregion
 
+        private System.Windows.Forms.Label Lproject;
+        private System.Windows.Forms.Label TProjectNama;
+        private System.Windows.Forms.Label LDeskripsi;
+        private System.Windows.Forms.Label LDosen;
+        private System.Windows.Forms.Label TDeskripsi;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label TDosen;
+        private System.Windows.Forms.Label LSkill;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
